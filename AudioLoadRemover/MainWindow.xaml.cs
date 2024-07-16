275db5300a2c9cfeb0b2e63b1ab276db130d7af2
Jabberrock <jabberrock@gmail.com>
2024-07-15 22:22:31 -0700

    Select and play video

--- AudioLoadRemover/MainWindow.xaml.cs
+++ AudioLoadRemover/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AudioLoadRemover
 {
@@ -19,6 +11,29 @@ namespace AudioLoadRemover
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenVideo_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Video files|*.mp4;*.mkv;*.avi";
+
+            var result = dialog.ShowDialog();
+            if (result ?? false)
+            {
+                var videoPath = dialog.FileName;
+
+                this.OpenVideoButton.Visibility = Visibility.Hidden;
+
+                this.VideoElement.Source = new Uri(videoPath);
+                this.VideoElement.Visibility = Visibility.Visible;
+
+                this.ProcessVideo(videoPath);
+            }
+        }
+
+        private void ProcessVideo(string videoPath)
+        {
         }
     }
 }
